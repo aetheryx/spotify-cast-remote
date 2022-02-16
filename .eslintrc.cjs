a1333108407c0e68d67a95f0f81f2093499a253b
aetheryx <zainalih3c@gmail.com>
2022-02-16 09:32:56 +0100

    disable no-namespace lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,7 @@ module.exports = {
     'no-extra-semi': 'warn',
     '@typescript-eslint/semi': 'warn',
     'quotes': ['warn', 'single'],
-    '@typescript-eslint/no-inferrable-types': ['off']
+    '@typescript-eslint/no-inferrable-types': ['off'],
+    '@typescript-eslint/no-namespace': ['off']
   },
 };
