59994c246786331234a9a92f6e65d1e95c73aaba
aetheryx <zainalih3c@gmail.com>
2022-02-17 01:18:29 +0100

    disable writable logic in prerender

--- src/routes/receiver/player-state.ts
+++ src/routes/receiver/player-state.ts
@@ -1,6 +1,11 @@
+import { browser } from '$app/env';
 import { writable } from 'svelte/store';
+import { backgroundURLInit } from './background-urls';
 import { castInit } from './cast-init';
 import type { PlayerState } from './parse-player-state';
 
 export const playerState = writable<PlayerState>(null);
-castInit(playerState);
+if (browser) {
+  castInit(playerState);
+  backgroundURLInit(playerState);
+}
