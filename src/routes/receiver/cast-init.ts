565920332d3c152a1c006fb6c41a00808b211382
aetheryx <zainalih3c@gmail.com>
2022-02-17 01:28:13 +0100

    remove browser check

--- src/routes/receiver/cast-init.ts
+++ src/routes/receiver/cast-init.ts
@@ -1,13 +1,8 @@
-import { browser } from '$app/env';
 import type { Writable } from 'svelte/store';
 import { Namespaces } from '../../ipc';
 import { intoState, type PlayerState } from './parse-player-state';
 
 export function castInit(playerState: Writable<PlayerState>): void {
-  if (!browser) {
-    return;
-  }
-
   const context = cast.framework.CastReceiverContext.getInstance();
 
   context.start({
@@ -20,7 +15,6 @@ export function castInit(playerState: Writable<PlayerState>): void {
   });
 
   context.addCustomMessageListener(Namespaces.PLAYER_STATE_SYNC, ({ data }) => {
-    console.log('received', data);
     playerState.set(intoState(data));
   });
 }
