b725245df1df53cfb6ad962cb2aa99ef6fdd8f15
aetheryx <zainalih3c@gmail.com>
2022-02-18 08:08:34 +0100

    add suggested component

---
+++ src/routes/receiver/player/suggested.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  const items: Array<{ imageURL: string }> = [
+    {
+      imageURL: 'https://t.scdn.co/images/3099b3803ad9496896c43f22fe9be8c4.png'
+    },
+    {
+      imageURL: 'https://newjams-images.scdn.co/image/ab676477000033ad/dt/v3/discover-weekly/62jut_pc1Y1cL40_cnlT2cIkNhzw5YsyELkfO4vduQVPl2PRmzELqwb4IPyxhB7YParfqrRnG9QJHie82MjEo6-fo-U-c9aEdBr4PhFr5vY=/MjU6MjQ6NzBUMjAtMzAtMg=='
+    },
+    {
+      imageURL: 'https://daily-mix.scdn.co/covers/backtracks/PZN_Repeat_Rewind2_DEFAULT-nl.jpg'
+    },
+    {
+      imageURL: 'https://daily-mix.scdn.co/covers/on_repeat/PZN_On_Repeat2_DEFAULT-nl.jpg',
+    },
+    {
+      imageURL: 'https://thisis-images.scdn.co/37i9dQZF1DZ06evO2nTSE0-default.jpg'
+    },
+    {
+      imageURL: 'https://i.scdn.co/image/ab6761610000517430d9a4acdf8cd3e8c0ad39ab',
+    },
+    {
+      imageURL: 'https://daily-mix.scdn.co/covers/time-capsule/time-capsule-blue_DEFAULT-nl.jpg',
+    },
+    {
+      imageURL: 'https://lineup-images.scdn.co/wrapped-2020-top100_DEFAULT-nl.jpg'
+    },
+    {
+      imageURL: 'https://lineup-images.scdn.co/wrapped-2021-top100_DEFAULT-nl.jpg'
+    },
+  ]
+</script>
+
+<div class="suggested">
+  {#each items as item}
+    <div class="item">
+      <img src={item.imageURL} alt="e" />
+    </div>
+  {/each}
+  <div class="item">
+    <div style="width: 30px" />
+  </div>
+</div>
+
+<style lang="scss">
+  .suggested {
+    display: flex;
+    flex-flow: row nowrap;
+    width: calc(100vw - 50px);
+    overflow: scroll;
+
+    .item {
+      display: flex;
+      flex-flow: column nowrap;
+      margin: 0 12px;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      img {
+        height: 160px;
+      }
+
+      div {
+        color: white;
+        font-family: spotify;
+        font-weight: 700;
+        font-size: 1em;
+      }
+    }
+  }
+</style>
